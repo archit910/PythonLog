2e667e1bad6865af7da33883dbe21206281e476b
archit910 <architchauhan910@gmail.com>
2019-03-04 00:16:43 +0530

    Passing By Value Printing all paths from root to leaves

--- Trees/BT/computation.py
+++ Trees/BT/computation.py
@@ -1,3 +1,4 @@
+import copy
 class TreeNode:
     '''
     This is a tree Node class
@@ -67,8 +68,54 @@ class BinaryTree():
             result = True
         return (result and lr and rr , ls + rs + root.key)
 
+    def count(self, root):
+        if not root:
+            return 0
+        return 1 + self.count(root.right) + self.count(root.left)
+
+    def divide_utility(self, root, flag , n):
+        if not root:
+            return 0
+        count = 1 + self.divide_utility(root.left, flag, n) + \
+                self.divide_utility(root.right, flag, n)
+        if count == n - count:
+            flag['flag'] = True
+        return count
+
+    def can_be_divided(self, root):
+
+        n = self.count(root)
+        flag = {'flag' : False}
+        self.divide_utility(root, flag, n)
+        return flag.get('flag')
+
+    def print_all_paths(self, root , paths):
+        if not root:
+            return
+        paths.append(root.key)
+        if self.is_leaf(root):
+            print " *** " , paths
+            print "\n"
+        self.print_all_paths(root.left, paths = copy.deepcopy(paths))
+        self.print_all_paths(root.right, paths = copy.deepcopy(paths))
+        # paths.pop() # just because lists are immutable in python
 
 
+    def print_all_paths_mut(self, root, paths):
+        if not root:
+            return
+        paths += " " + str(root.key)
+        if self.is_leaf(root):
+            print paths , "\n"
+        self.print_all_paths_mut(root.left, paths)
+        self.print_all_paths_mut(root.right, paths)
+
+
+def areMirror(root1, root2):
+    if not (root1 and root2):
+        return True
+    return root2.key == root1.key and areMirror(root1.left, root2.right) \
+            and areMirror(root1.right, root2.left)
 
 if __name__ == '__main__':
     # n1 = TreeNode(10)  # root
@@ -91,11 +138,14 @@ if __name__ == '__main__':
     n1.left = n2
     n1.right = n3
     n2.left = n4
-    n2.right = n5
+    n2.right = n5 # comment it to split a tree into two halves
     n3.left = n6
     n3.right = n7
 
     tree = BinaryTree(root=n1)  # n1 as a root
-    print tree.children_sum(root=tree.root)
-    print "\n"
-    print tree.sum_tree(root=tree.root)
+    # print tree.children_sum(root=tree.root)
+    # print "\n"
+    # print tree.sum_tree(root=tree.root)
+    # print tree.can_be_divided(tree.root)
+    tree.print_all_paths(tree.root, [])
+    tree.print_all_paths_mut(tree.root, '')
